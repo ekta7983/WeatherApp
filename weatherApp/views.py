e2c2a80b48c10a96804563ec5a30e96dc15b4e83
ekta79 <ekta.2226cse1019@kiet.edu>
2025-06-19 13:55:56 +0530

    Initial commit - Weather Application

---
+++ weatherApp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+
+
+def home(request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'meerut'
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=4d7ec0f45ead664bc3d1903d8638ed4a' 
+    PARAMS = {'units':'metric'}
+
+    API_KEY ='AIzaSyCErevNQjx3UxmuJZhbPamDsMVg85BaRfk'
+    SEARCH_ENGINE_ID = '5299cbc309d914b39'
+
+    query = city + " 1920x1080"
+    page = 1
+    start = (page - 1) * 10 + 1
+    searchType ='image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = requests.get(city_url).json()
+    count = 1
+    search_items = data.get("items")
+    image_url = search_items[1]['link']
+
+    
+
+    try:
+        data = requests.get(url,params=PARAMS).json()
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        day = datetime.date.today()
+
+        return render(request,'weatherapp/index.html',{'description':description,'icon':icon,'temp':temp,'day':day,'city':city, 'exception_occurred':False ,'image_url':image_url})
+    
+    except KeyError:
+        exception_occured = True
+        messages.error(request,'entered data is not available to API')
+        day=datetime.date.today()
+
+        return render(request,'weatherapp/index.html',{'description':'clear sky','icon':'01d','temp':25,'day':day,'city':'Meerut','exception_occured':exception_occured})
+
+    
